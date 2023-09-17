4f58d508f6828324c9d69b128f19d607c66fd726
Adam Brooks <mistradam@icloud.com>
2023-09-17 17:01:12 +0800

    eycie: revision update -- 4 * Added loading screen everytime changing pages, thanks to Adam Brooks of codepen (as well as in github). * Removed pages directory, and moved all htmls in the same directory of the index, no oarticular reason. * To properly load/unload the loading screen, had to use javascript that listen in DOM load, then change some css properties.
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,12 @@
-window.addEventListener("load", () => {
-    const loader = document.querySelector(".loader");
-
-    // Add the "loader-hidden" class
-    loader.classList.add("loader-hidden");
-
-    // Check if the "loader-hidden" class has been added
-    if (loader.classList.contains("loader-hidden")) {
-        console.log("loader-hidden class has been added");
-    }
-
-    loader.addEventListener("transitionend", () => {
-        document.body.removeChild(document.querySelector(".loader"));
-    });
+document.addEventListener('DOMContentLoaded', function() {
+    // Set loader and content class
+    const loader = document.querySelector("#loader");
+    const content = document.querySelector("#content");
+    
+    // Apply changes in display whenever index page loaded up
+    setTimeout(function() {
+        loader.style.display = "none";
+        loader.style.opacity = "0";
+        content.style.display = "block";
+    }, 4500);
 });
